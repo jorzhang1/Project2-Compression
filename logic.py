1234ea5094bc76e3e2b6949c62d8fb1a6f86059e
Jordan Zhang <jordanzhang@unomaha.edu>
2024-05-04 22:08:05 -0500

    Changed code

---
+++ logic.py
@@ -0,0 +1,29 @@
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QTextEdit, QFileDialog)
+from video_gui import *
+import os
+from PIL import Image
+
+class Logic(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.pushButton.clicked.connect(self.getFileName)
+        self.pushButton_3.clicked.connect(self.compress)
+
+        self.filename = ''
+
+    def getFileName(self):
+        file_filter = 'Image File (*.jpg, *.png)'
+        response = QFileDialog.getOpenFileName(
+            parent=self,
+            caption='Select a file',
+            directory=os.getcwd(),
+            filter=file_filter,
+            initialFilter='Image File (*.jpg, *.png)'
+        )
+        self.filename = str(response)
+        print(self.filename)
+
+    def compress(self):
+     image = Image.open(self.filename)
