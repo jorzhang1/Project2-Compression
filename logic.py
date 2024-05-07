e399ac0a39f8caca34aa49a9022821ad51461c3a
EduardoPerez04 <90478811+EduardoPerez04@users.noreply.github.com>
2024-05-07 15:59:49 -0500

    howfully it works for u

--- logic.py
+++ logic.py
@@ -29,8 +29,8 @@ class Logic(QMainWindow, Ui_MainWindow):
 
         self.filenames = ''
 
-        #radio stuff bruh
-        #self.quality = self.
+        #quality
+        self.quality = None
 
         self.width = None
         self.height = None
@@ -51,6 +51,19 @@ class Logic(QMainWindow, Ui_MainWindow):
         if self.filenames:
             file_type = self.filenames[0][-4:-1]
 
+            if self.thirty_radio.isChecked():
+                self.quality = 30
+            elif self.fifty_radio.isChecked():
+                self.quality = 50
+            elif self.seventy_radio.isChecked():
+                self.quality = 70
+            elif self.ninety_radio.isChecked():
+                self.quality = 90
+            else:
+                self.label_2.setText('Please select any compress ratio')
+                self.setStyleSheet("QRadioButton::indicator:!checked { color: rgb(170, 0, 0); }")
+                return
+
             if (file_type == 'png' or file_type == 'jpg') and (self.x_value.text().isdigit()
                                                                and self.y_value.text().isdigit()):
                 self.width = int(self.x_value.text())
@@ -59,7 +72,7 @@ class Logic(QMainWindow, Ui_MainWindow):
                 image = Image.open(self.filenames[0][2:-1])
                 new_size = (self.width, self.height)
                 resized_image = image.resize(new_size)
-                resized_image.save(self.new_filename, optimize=True, quality=50)
+                resized_image.save(self.new_filename, optimize=True, quality=self.quality)
                 original_size = os.path.getsize(self.filenames[0][2:-1])
                 compressed_size = os.path.getsize(self.new_filename)
 
@@ -74,6 +87,7 @@ class Logic(QMainWindow, Ui_MainWindow):
 
             else:
                 self.label_2.setText('Fill all options; dimensions must be numeric')
+
 
         else:
             self.label_2.setText('Upload a file')
