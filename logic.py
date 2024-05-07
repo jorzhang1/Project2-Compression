16d18648895b0f29ac81118f50292508c7f18ff9
Jordan Zhang <jordanzhang@unomaha.edu>
2024-05-06 23:38:39 -0500

    Fixed image errors

--- logic.py
+++ logic.py
@@ -26,23 +26,24 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.new_filename = ''
 
     def getFileName(self):
-        file_filter = 'Image File (*.png, *.jpg);; Video File (*.mp4);; Audio File (*.mp3)'
+        file_filter = 'Image File (*.png *.jpg);; Video File (*.mp4);; Audio File (*.mp3)'
         response = QFileDialog.getOpenFileName(
             parent=self,
             caption='Select a file',
-            directory=os.getcwd(),
             filter=file_filter,
         )
         self.filenames = str(response).split(',')
 
     def compress(self):
+
         if self.filenames:
             file_type = self.filenames[0][-4:-1]
-            self.width = int(self.x_value.text())
-            self.height = int(self.y_value.text())
-            self.new_filename = str(self.new_name.text()) + '.' + str(file_type)
 
-            if file_type == 'png' or file_type == 'jpg':
+            if (file_type == 'png' or file_type == 'jpg') and (self.x_value.text().isdigit()
+                                                               and self.y_value.text().isdigit()):
+                self.width = int(self.x_value.text())
+                self.height = int(self.y_value.text())
+                self.new_filename = str(self.new_name.text()) + '.' + str(file_type)
                 image = Image.open(self.filenames[0][2:-1])
                 new_size = (self.width, self.height)
                 resized_image = image.resize(new_size)
@@ -54,8 +55,13 @@ class Logic(QMainWindow, Ui_MainWindow):
                 print("Compressed Size: ", compressed_size)
 
             elif file_type == 'mp4':
+                print("hi")
 
             elif file_type == 'mp3':
+                print('whats good')
+
+            else:
+                self.label_2.setText('Fill all options; dimensions must be numeric')
 
         else:
             self.label_2.setText('Upload a file')
