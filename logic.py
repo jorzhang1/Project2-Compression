1d408e2ade50ffa2b979d179d49ff45faabff0e3
Jordan Zhang <jordanzhang@unomaha.edu>
2024-05-06 23:08:43 -0500

    compress images progress

--- logic.py
+++ logic.py
@@ -15,7 +15,15 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.select_push.clicked.connect(self.getFileName)
         self.compress_push.clicked.connect(self.compress)
 
-        self.filename = ''
+        self.filenames = ''
+
+        #radio stuff bruh
+        #self.quality = self.
+
+        self.width = None
+        self.height = None
+
+        self.new_filename = ''
 
     def getFileName(self):
         file_filter = 'Image File (*.png, *.jpg);; Video File (*.mp4);; Audio File (*.mp3)'
@@ -25,16 +33,29 @@ class Logic(QMainWindow, Ui_MainWindow):
             directory=os.getcwd(),
             filter=file_filter,
         )
-        self.filename = str(response).split(',')
+        self.filenames = str(response).split(',')
 
     def compress(self):
-        if self.filename:
-            file_type = self.filename[0][-4:-1]
+        if self.filenames:
+            file_type = self.filenames[0][-4:-1]
+            self.width = int(self.x_value.text())
+            self.height = int(self.y_value.text())
+            self.new_filename = str(self.new_name.text()) + '.' + str(file_type)
 
             if file_type == 'png' or file_type == 'jpg':
-                image = Image.open(self.filename[0][2:-1])
-                width, height = image.size
-                print(width, height)
-                new_size = 0
+                image = Image.open(self.filenames[0][2:-1])
+                new_size = (self.width, self.height)
+                resized_image = image.resize(new_size)
+                resized_image.save(self.new_filename, optimize=True, quality=50)
+                original_size = os.path.getsize(self.filenames[0][2:-1])
+                compressed_size = os.path.getsize(self.new_filename)
+
+                print("Original Size: ", original_size)
+                print("Compressed Size: ", compressed_size)
+
+            elif file_type == 'mp4':
+
+            elif file_type == 'mp3':
+
         else:
             self.label_2.setText('Upload a file')
