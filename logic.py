200ea39a21f8dc65dcbb4d91f29f88a0a658da9c
Jordan Zhang <jordanzhang@unomaha.edu>
2024-05-09 22:31:36 -0500

    Error handling, type hinting, and documentation

--- logic.py
+++ logic.py
@@ -8,48 +8,72 @@ from PIL import Image
 
 
 class Start(QMainWindow, Ui_StartWindow):
-    def __init__(self):
+    """
+    Class that builds the start window
+    and contains a method to switch windows
+    """
+    def __init__(self) -> None:
+        """
+        Method that sets up the ui when the program is run
+        """
         super().__init__()
         self.setupUi(self)
 
         self.start_push.clicked.connect(self.changeWindow)
 
-    def changeWindow(self):
+    def changeWindow(self) -> None:
+        """
+        Method that switches windows to compression screen when button
+        is pressed
+        """
         self.window = Logic()
         self.window.show()
         self.close()
 
 
 class Logic(QMainWindow, Ui_MainWindow):
-    def __init__(self):
+    """
+    Class that builds the main window
+    and contains methods that can compress media files
+    """
+    def __init__(self) -> None:
+        """
+        Method that sets up ui when the program is run
+        Also calls other methods when buttons are pressed
+        """
         super().__init__()
         self.setupUi(self)
         self.select_push.clicked.connect(self.getFileName)
         self.compress_push.clicked.connect(self.compress)
-        self.filenames = ''
+        self.filenames: str = ''
 
-        # quality
         self.quality = None
 
         self.width = None
         self.height = None
 
-        self.new_filename = ''
+        self.new_filename: str = ''
 
-    def getFileName(self):
-        file_filter = 'Image File (*.png *.jpg);; Video File (*.mp4);; Audio File (*.mp3)'
-        response = QFileDialog.getOpenFileName(
+    def getFileName(self) -> None:
+        """
+        Method that prompts the user to select a file from their computer
+        """
+        file_filter: str = 'Image File (*.png *.jpg);; Video File (*.mp4);; Audio File (*.mp3)'
+        response: tuple = QFileDialog.getOpenFileName(
             parent=self,
             caption='Select a file',
             filter=file_filter,
         )
-        self.filenames = str(response).split(',')
+        self.filenames: list = str(response).split(',')
         self.change_labels()
 
-    def compress(self):
-
+    def compress(self) -> None:
+        """
+        Method that changes the file size and quality of the file
+        the user selected and makes a new file in the working directory
+        """
         if self.filenames:
-            file_type = self.filenames[0][-4:-1]
+            file_type: str = self.filenames[0][-4:-1]
 
             if (file_type == 'png' or file_type == 'jpg') and (self.x_value.text().isdigit()
                                                                and int(self.x_value.text()) > 0
@@ -71,9 +95,9 @@ class Logic(QMainWindow, Ui_MainWindow):
                 self.width = int(self.x_value.text())
                 self.height = int(self.y_value.text())
                 if self.width < 3840 and self.height < 2160:
-                    self.new_filename = str(self.new_name.text()) + '.' + str(file_type)
+                    self.new_filename: str = str(self.new_name.text()) + '.' + str(file_type)
                     image = Image.open(self.filenames[0][2:-1])
-                    new_size = (self.width, self.height)
+                    new_size: tuple = (self.width, self.height)
                     resized_image = image.resize(new_size)
                     resized_image.save(self.new_filename, optimize=True, quality=self.quality)
 
@@ -97,18 +121,19 @@ class Logic(QMainWindow, Ui_MainWindow):
                     self.label_2.show()
                 else:
                     if 0 <= quality <= 51:
-                        input_path = self.filenames[0][2:-1]
+                        input_path: str = self.filenames[0][2:-1]
                         self.new_filename = str(self.new_name.text()) + '.' + str(file_type)
-                        output_path = self.new_filename
-                        ffmpeg_cmd = f'ffmpeg -i {input_path} -c:v libx264 -c:a copy -crf {quality} {output_path}'
+                        output_path: str = self.new_filename
+                        ffmpeg_cmd: str = f'ffmpeg -i {input_path} -c:v libx264 -c:a copy -crf {quality} {output_path}'
                         subprocess.run(ffmpeg_cmd, shell=True)
 
-                        # the quality in the video command is the -crf 20. The range is 0-51 where 0 is lossless,
-                        # 23 is default,
-                        # and 51 is the worst quality.
+                        """
+                        The quality in the video command is the -crf 20. The range is 0-51 where 0 is lossless,
+                        23 is default, and 51 is the worst quality.
+                        """
 
-                        original_size = os.path.getsize(self.filenames[0][2:-1])
-                        compressed_size = os.path.getsize(self.new_filename)
+                        original_size: int = os.path.getsize(self.filenames[0][2:-1])
+                        compressed_size: int = os.path.getsize(self.new_filename)
 
                         self.label_2.setText(f'Compression complete; Check working directory\n'
                                              f'\nOriginal size: {original_size}\nNew size: {compressed_size}')
@@ -130,15 +155,15 @@ class Logic(QMainWindow, Ui_MainWindow):
                     self.label_2.show()
                 else:
                     if 32 <= quality <= 320:
-                        input_path = self.filenames[0][2:-1]
-                        output_path = self.new_filename
-                        ffmpeg_cmd = f'ffmpeg -i {input_path} -c:a libmp3lame -b:a {quality}k {output_path}'
+                        input_path: str = self.filenames[0][2:-1]
+                        output_path: str = self.new_filename
+                        ffmpeg_cmd: str = f'ffmpeg -i {input_path} -c:a libmp3lame -b:a {quality}k {output_path}'
                         subprocess.run(ffmpeg_cmd, shell=True)
 
-                        # the quality in the audio command is the 128k. The range is typically 32 to 320 kilobits.
+                        # The quality in the audio command is the 128k. The range is typically 32 to 320 kilobits.
 
-                        original_size = os.path.getsize(self.filenames[0][2:-1])
-                        compressed_size = os.path.getsize(self.new_filename)
+                        original_size: int = os.path.getsize(self.filenames[0][2:-1])
+                        compressed_size: int = os.path.getsize(self.new_filename)
 
                         self.label_2.setText(f'Compression complete; Check working directory\n'
                                              f'\nOriginal size: {original_size}\nNew size: {compressed_size}')
@@ -152,8 +177,12 @@ class Logic(QMainWindow, Ui_MainWindow):
             self.label_2.setText('Upload a file')
             self.label_2.show()
 
-    def change_labels(self):
-        file_type = self.filenames[0][-4:-1]
+    def change_labels(self) -> None:
+        """
+        Method that changes the labels and input areas depending on
+        the file type the user selects
+        """
+        file_type: str = self.filenames[0][-4:-1]
         if file_type == 'jpg' or file_type == 'png':
 
             self.ratio_label.show()
